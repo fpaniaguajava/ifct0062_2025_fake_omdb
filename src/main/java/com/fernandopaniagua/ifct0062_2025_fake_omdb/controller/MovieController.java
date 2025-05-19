5e4b727aaa9b8b6931b37c010fa7d7cbe7ef952c
fpaniaguajava <fernando.paniagua.java@gmail.com>
2025-05-19 19:38:58 +0200

    Búsqueda por diversos criterios

--- src/main/java/com/fernandopaniagua/ifct0062_2025_fake_omdb/controller/MovieController.java
+++ src/main/java/com/fernandopaniagua/ifct0062_2025_fake_omdb/controller/MovieController.java
@@ -2,10 +2,14 @@ package com.fernandopaniagua.ifct0062_2025_fake_omdb.controller;
 
 import com.fernandopaniagua.ifct0062_2025_fake_omdb.model.Movie;
 import com.fernandopaniagua.ifct0062_2025_fake_omdb.service.interfaces.MovieService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,9 +29,31 @@ public class MovieController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getMovieById(@PathVariable String id) {
         Movie movie = movieService.findById(id);
+        if (movie==null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok().body(Map.of(
                 "data", movie,
                 "message", "Movie found"
         ));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List> findMovies(@RequestParam(name="s", required = false) String titles,
+                                           @RequestParam(name="i", required = false) String imdbId,
+                                           @RequestParam(name="t", required = false) String title) {
+        if (titles!=null) {
+            return ResponseEntity.ok().body(movieService.findMoviesByTitle(titles));
+        } else if (imdbId !=null) {
+            List<Movie> movies = new ArrayList();
+            movies.add(movieService.findById(imdbId));
+            return ResponseEntity.ok().body(movies);
+        } else if (title != null) {
+            List<Movie> movies = new ArrayList();
+            movies.add(movieService.findByTitle(title));
+            return ResponseEntity.ok().body(movies);
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request");
+        }
     }
 }
---
+++ src/main/java/com/fernandopaniagua/ifct0062_2025_fake_omdb/repository/IAutomaticRestRepository.java
@@ -0,0 +1,11 @@
+package com.fernandopaniagua.ifct0062_2025_fake_omdb.repository;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * Este repository se utiliza para exponer el API REST
+ * automático proporcionado por SPRING DATA REST
+ */
+@RepositoryRestResource
+public class IAutomaticRestRepository {
+}
